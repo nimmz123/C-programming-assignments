bc45466e570126b7063b0e1d93967b9f511c8070
nimmz123 <62934782+nimmz123@users.noreply.github.com>
2020-03-31 16:02:44 +0530

    First assignment

---
+++ details.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+    printf("Nimmy John\nElectronics and Communication\nCEC");
+    return 0;
+}
---
+++ welcome to learn to code.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+    printf("Welcome to learn to code");
+    return 0;
+}
