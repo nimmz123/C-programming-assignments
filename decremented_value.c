aea2bbacedcf4bf892cca705151031dd4996c6ea
Nimmy John <62934782+nimmz123@users.noreply.github.com>
2020-04-05 23:13:03 +0530

    Add files via upload

---
+++ decremented_value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    printf ("Enter the number:");
+    scanf("%d", &n);
+    do
+{
+         printf ("%d \n",n);
+         n--;
+ }
+    while(n >= 0);
+    return 0;
+ }
+ 
